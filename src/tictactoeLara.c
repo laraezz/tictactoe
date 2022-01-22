8fdcd91cdbd373277bdebb35b1d75d91b756bffc
Lara Ezze <97916338+laraezz@users.noreply.github.com>
2022-01-21 21:25:03 -0500

    Update tictactoeLara.c

--- src/tictactoeLara.c
+++ src/tictactoeLara.c
@@ -1,24 +1,14 @@
-/************************ezzeddineLaraA2.c**************
-Student Name: Lara Ezzeeddine Email Id: lezzeddi
-Due Date: November 11th Course Name: CIS 1300
-I have exclusive control over this submission via my password.
-By including this statement in this header comment, I certify that:
-1) I have read and understood the University policy on academic integrity;
-2) I have completed the Computing with Integrity Tutorial on Moodle; and
-3) I have achieved at least 80% in the Computing with Integrity Self Test.
-I assert that this work is my own. I have appropriately acknowledged any and all material that I have used, whether directly quoted or paraphrased. Furthermore, I certify that this assignment was prepared by me specifically for this course.
-
-
+/*
 Compiling the program
 The program should be compiled using the following flags: -std=c99 -Wall
 compiling:
-gcc givenA2.c tictactoeLara.c  -std=c99 -Wall
+gcc maintictactoeLara.c tictactoeLara.c headertictactoeLara.h -std=c99 -Wall
 Running the Program Running: ./a.out
 
 
 */
 
-#include "givenA2.h"
+#include "headertictactoeLara.h"
 
 void createInitialBoard (char board[N][N]) {
   
